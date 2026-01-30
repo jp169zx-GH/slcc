ec4126e2701e145f1ce6937cd3c12b9d7b70d636
jp169zx-GH <jp169zx@gmail.com>
2026-01-29 16:06:43 -0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,40 @@
-const CACHE_NAME = 'sllcc-v1';
+const CACHE_NAME = 'slcc-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/slcc/',
+  '/slcc/index.html',
+  '/slcc/manifest.json',
+  '/slcc/icon-152.png',
+  '/slcc/icon-167.png',
+  '/slcc/icon-180.png',
+  '/slcc/icon-192.png',
+  '/slcc/icon-512.png'
 ];
 
-// 安裝事件 - 緩存資源
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('已打開緩存');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch事件 - 離線時從緩存提供服務
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // 緩存命中 - 返回響應
         if (response) {
           return response;
         }
         return fetch(event.request);
-      }
-    )
+      })
   );
 });
 
-// 激活事件 - 清理舊緩存
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
